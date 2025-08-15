ae8b3a9252671a8447e1b50d4b032237c6798257
HishamBoghdady <hishamboghdad@gmail.com>
2025-08-16 02:43:01 +0300

    edit Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,14 @@ export default function Intro() {
       <div style={{ direction: "ltr" }}>
         
         <h1>Welcome in Health Center</h1>
-        {/* <span>created by emad younis</span> */}
+
+          <div style={{display: 'flex',flexWrap: 'wrap',gap: '0',justifyContent: 'center',
+            alignItems: 'center',borderRadius: '12px',overflow: 'hidden',boxShadow: '0 4px 15px rgba(0,0,0,0.2)'}}>
+            <img src="/Elmanar.jpg" alt="Elmanar" style={{flex: '1 1 300px',width: '10%',height: 'auto',objectFit: 'cover'}} />
+
+            <img src="/الادمان.png" alt="الادمان" style={{flex: '1 1 300px',width: '10%',height: 'auto',objectFit: 'cover'}} />
+          </div>
+
       </div>
     </>
   )
